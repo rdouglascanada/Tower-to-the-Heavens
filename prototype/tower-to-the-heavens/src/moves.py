efce681815b6f22cf6939466347d2803f94f2894
Richard Douglas <rdouglascanada@gmail.com>
2018-08-18 17:14:19 -0400

    Refactor moves class to be more maintainable

--- prototype/tower-to-the-heavens/src/moves.py
+++ prototype/tower-to-the-heavens/src/moves.py
@@ -4,24 +4,12 @@ moves.py
 @author: Richard Douglas
 Created on Aug 18, 2018
 '''
-from copy import deepcopy
-
-
 class Move:
-    def __init__(self, name, cost, damage):
-        self._name = name
-        self._cost = cost
-        self._damage = damage
+    def __init__(self, args):
+        self.name = lambda: args['name']
+        self.cost = lambda: args['cost']
+        self.damage = lambda: args['damage']
         return
-    
-    def name(self):
-        return self._name
-    
-    def cost(self):
-        return self._cost
-    
-    def damage(self):
-        return self._damage
 
 class MoveNames:
     ATTACK = "Attack"
@@ -51,9 +39,9 @@ class MoveFactory:
     def __init__(self, data):
         self._templates = {}
         for move_data in data:
-            move = Move(move_data['name'], move_data['cost'], move_data['damage'])
+            move = Move(move_data)
             self._templates[move_data['name']] = move
         return
     
     def getMove(self, name):
-        return deepcopy(self._templates[name])
+        return self._templates[name]
