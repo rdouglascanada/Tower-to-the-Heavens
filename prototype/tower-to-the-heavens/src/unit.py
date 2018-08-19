003ab2c77b9a2111c9c6adbfc941c2fde2134294
Richard Douglas <rdouglascanada@gmail.com>
2018-08-18 21:27:10 -0400

    Add max_hitpoints and power attributes to Unit class

--- prototype/tower-to-the-heavens/src/unit.py
+++ prototype/tower-to-the-heavens/src/unit.py
@@ -7,6 +7,8 @@ from move import MoveNames
 class Unit:
     def __init__(self, args):
         self.name = lambda: args['name']
+        self.max_hitpoints = lambda: args['max_hitpoints']
+        self.power = lambda: args['power']
         self.moves = lambda: tuple(args['moves'])
         return
 
@@ -18,10 +20,14 @@ class UnitData:
     data = [
             {
                 'name': UnitNames.FIRE_MAGE,
+                'max_hitpoints': 10,
+                'power': 10,
                 'move_names': [MoveNames.HOMING_FIRE],
             },
             {
                 'name': UnitNames.BATUMAN,
+                'max_hitpoints': 30,
+                'power': 20,
                 'move_names': [MoveNames.BLINDING_DARKNESS],
             },
         ]
--- prototype/tower-to-the-heavens/src/unit_utest.py
+++ prototype/tower-to-the-heavens/src/unit_utest.py
@@ -14,10 +14,14 @@ class Test(unittest.TestCase):
         
         fire_mage = factory.getUnit(UnitNames.FIRE_MAGE)
         self.assertEqual(UnitNames.FIRE_MAGE, fire_mage.name())
+        self.assertEqual(10, fire_mage.max_hitpoints())
+        self.assertEqual(10, fire_mage.power())
         self.assertEqual((MoveNames.HOMING_FIRE,), tuple(mv.name() for mv in fire_mage.moves()))
         
         batuman = factory.getUnit(UnitNames.BATUMAN)
         self.assertEqual(UnitNames.BATUMAN, batuman.name())
+        self.assertEqual(30, batuman.max_hitpoints())
+        self.assertEqual(20, batuman.power())
         self.assertEqual((MoveNames.BLINDING_DARKNESS,), tuple(mv.name() for mv in batuman.moves()))
         return
 
