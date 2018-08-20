1fc001c104e8f9c542d807811e433a010b360df4
Richard Douglas <rdouglascanada@gmail.com>
2018-08-19 21:54:30 -0400

    Add ability to display levels to text application

--- prototype/tower-to-the-heavens/src/text_application/main.py
+++ prototype/tower-to-the-heavens/src/text_application/main.py
@@ -2,14 +2,26 @@
 @author: Richard Douglas
 Created on Aug 19, 2018
 '''
+from data import get_data_manager
+
+
 class Commands:
     QUIT = "-1"
-    PRINT_COMMANDS = "-2"
+    LEVELS = "0"
+    PRINT_COMMANDS = "1"
+    
 
 def print_commands():
     print("Commands")
     print("{0} - Quit the game".format(Commands.QUIT))
+    print("{0} - Display Levels".format(Commands.LEVELS))
     print("{0} - Print commands".format(Commands.PRINT_COMMANDS))
+    return
+
+def print_levels():
+    data_manager = get_data_manager()
+    print("Levels")
+    print(tuple(lv.name() for lv in data_manager.getLevels()))
     return
 
 def prompt_for_input():
@@ -21,7 +33,9 @@ print_commands()
 print
 user_input = prompt_for_input()
 while user_input != Commands.QUIT:
-    if user_input == Commands.PRINT_COMMANDS:
+    if user_input == Commands.LEVELS:
+        print_levels()
+    elif user_input == Commands.PRINT_COMMANDS:
         print_commands()
     else:
         print("Invalid command {0}".format(user_input))
